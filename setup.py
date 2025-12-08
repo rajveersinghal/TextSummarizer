9e37d2af954dbb4016654c12b2adf6d118a4e5b9
Rajveer Singhal <singhalrajveer7@gmail.com>
2025-12-08 14:08:47 +0530

    Requirement.txt Added

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "1.0.0"
+REPO_NAME = "TextSummarizer"
+AUTHOR_USER_NAME = "rajveersinghal"
+SRC_REP="TextSummarizer"
+AUTHOR_EMAIL = "singhalrajveer7@gmail.com"
